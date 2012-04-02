4f6f85af8fcd1c0ffec34c68f148d648f35053be
Mike Prendergast <mikeprendergast@gmail.com>
2012-04-02 18:53:34 -0400

    messing with gitignore

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-  validates_presence_of :first_name, :last_name
-  validates :email, :presence => true, :uniqueness => true, :email_format => true
-  acts_as_authentic
-end
--- test/unit/kibera_child_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class KiberaChildTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
